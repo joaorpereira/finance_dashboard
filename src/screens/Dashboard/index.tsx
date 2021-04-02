8c677a373bbef9b4df3d8a4de348279d73a5bca1
joao paulo <jprp17@live.com>
2021-04-02 14:31:58 -0300

    feat(saidas): page saidas concluded

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,29 +1,55 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import Select from '../../components/Select'
 import HeaderTitle from '../../components/HeaderTitle'
 import monthList from '../../utils/monthList'
 import yearsOptions from '../../mocks/yearsOptions'
 
 import * as S from './styled'
+import { current_month, current_year } from '../../utils/constants'
 
 const Dashboard: React.FC = () => {
+  const [month, setMonth] = useState<string>(current_month)
+  const [year, setYear] = useState<string>(current_year)
+  const [status, setStatus] = useState<string>('')
+
+  const monthOptions = useMemo(() => {
+    const list = monthList
+      .map((month, index) => ({
+        value: index + 1,
+        option: month,
+      }))
+      .filter(month =>
+        Number(year) === Number(current_year)
+          ? Number(month.value) <= Number(current_month)
+          : month
+      )
+
+    return list
+  }, [year])
+
   return (
     <S.Wrapper>
       <HeaderTitle title={'Dashboard'} color={'#222831'}>
-        {/* <Select
+        <Select
           options={monthOptions}
           name='date'
           label='Mês'
-          onChange={() => undefined}
+          defaultValue={month}
+          onChange={e => setMonth(e.target.value)}
         />
         <Select
           options={yearsOptions}
           name='ano'
           label='Ano'
-          onChange={() => undefined}
-        /> */}
+          defaultValue={year}
+          onChange={e => setYear(e.target.value)}
+        />
       </HeaderTitle>
-      <S.Main></S.Main>
+      <S.Main>
+        <S.Row>
+            
+        </S.Row>
+      </S.Main>
     </S.Wrapper>
   )
 }
